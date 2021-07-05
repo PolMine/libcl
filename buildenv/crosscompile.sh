9f5e8dbd5ec07929d11cb8741ba377356f4ee39e
Andreas Blaette <andreas.blaette@uni-due.de>
2021-07-05 16:01:03 +0200

    dependencies compiled

--- buildenv/crosscompile.sh
+++ buildenv/crosscompile.sh
@@ -4,6 +4,14 @@ ARCHITECTURES=( x86_64 i686 )
 for ARCH in ${ARCHITECTURES[@]}
 do
   echo $ARCH
-  docker build -t cwbcross:$ARCH --build-arg TARGET=$ARCH .
-  docker run -it --rm --env TARGET=$ARCH -v ~/Lab/github/libcl:/libcl cwbcross:$ARCH
+  docker build \
+    -t cwbcross:$ARCH \
+    --build-arg TARGET=$ARCH \
+    --build-arg MINGWDIR=/usr/lib/gcc/$ARCH-w64-mingw32/4.8/ \
+    .
+  docker run -it --rm \
+    --env TARGET=$ARCH \
+    --env MINGWDIR=/usr/lib/gcc/$ARCH-w64-mingw32/4.8/ \
+    -v ~/Lab/github/libcl:/libcl \
+    cwbcross:$ARCH
 done
